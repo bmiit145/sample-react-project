eb85dff91f9e0c89dd47f19c6004c69e58a23d0f
bmiit145 <21bmiit145@gmail.com>
2023-09-24 15:50:51 +0530

    gallery

--- app/Gallery/page.js
+++ app/Gallery/page.js
@@ -1,7 +1,50 @@
-import React from 'react';
-
+"use client"
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 const gallery = () => {
 
+    // usestate variable
+    const [Images, SetImages] = useState([]);
+
+    // for api 
+    const getImages = async () => {
+        try {
+            const response = await axios.get("https://picsum.photos/v2/list")
+            const data = response.data
+            console.log(data);
+            // SetImages(data)
+        } catch (error) {
+            console.error("Error while fetch");
+        }
+    }
+
+    // useEffect for direct calling without click
+
+    useEffect(() => {
+        getImages();
+    }, [])
+
+
+    return (
+        <div className='p-10'>
+
+            {/* <button onClick={getImages} className=' px-5 py-3 bg-green-600 text-white  font-bold'>
+                Get Images
+            </button> */}
+            <button onClick={() => {
+                SetImages(data)
+            }} className=' px-5 py-3 bg-green-600 text-white  font-bold'>
+                Get Images
+            </button>
+            <div className='p-10'>
+                {
+                    Images.map((img, i) => {
+                        return <img key={i} src={img.download_url} width={300} height={300} className='m-10 rounded inline-block' />
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 export default gallery
